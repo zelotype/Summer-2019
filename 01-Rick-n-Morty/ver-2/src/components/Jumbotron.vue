e76925d3cca2f6dd0b9b76e3e3ab39b631a4b16b
Kawisara  Bunyen <zelotype.3@gmail.com>
2019-06-06 17:26:16 +0700

    can use font

--- 01-Rick-n-Morty/ver-2/src/components/Jumbotron.vue
+++ 01-Rick-n-Morty/ver-2/src/components/Jumbotron.vue
@@ -35,6 +35,48 @@ export default {
 }
 </script>
 
+<style>
+@font-face {
+    font-family: classique;
+    src: url(../assets/font/Cavorting.ttf);
+}
+.jumbotron{
+    text-align: center;
+    position: relative;
+    padding-top: 0%;
+}
+
+.black-jumbotron{
+    width: 100%; 
+    height: 100%;
+    position: absolute;
+    background-color: rgba(0,0,0,0.6);
+}
+
+.head{
+  padding-top: inherit;
+    margin: auto;
+    background-repeat: no-repeat;
+    height: 1000px;
+    background-image: url("https://vignette.wikia.nocookie.net/rickandmorty/images/9/98/S2e3_mount_morty_and_summer.png/revision/latest?cb=20160923231412");
+    width: 100%;
+}
+
+.text-head{
+    text-align: center;
+    padding-top: 10%;
+    color: white;
+}
+
+.display-4{
+    padding-bottom: 0px;
+    font-size: 100px;
+    font-family: classique;
+}
+
+</style>
+
+
 
 
 
